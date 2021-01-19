d110002d8b97e257cc2347c9e95dc2893f6edee5
Olivia Lewis <ollybaby12@gmail.com>
2021-01-18 23:25:57 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import react from 'react';
+import React from 'react';
+import List from './List';
+import './App.css';
+
+class App extends React.Component {
+  static defaultProps = {
+    store:{
+      list:[],
+      allCards:{},
+    }
+  };
+
+  render(){
+    const {store} = this.props
+    console.log(store)
+  return (
+    <main className='App'>
+      <header className='App-header'>
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className='App-list'>
+        {store.lists.map(list =>(
+          <List
+            key={list.id}
+            header={list.header}
+            cards={list.cardIds.map(id => store.allCards[id])}
+          />
+        ))}
+      </div>
+    </main>
+    );
+  }
+}
+
+export default App;
